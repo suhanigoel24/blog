82fb82221b8e1fefbf20a51ff467531693f93aa9
suhanigoel24 <suhanigoel03@gmail.com>
2025-06-25 02:33:17 +0530

    told ya kiddo

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     store: MongoStore.create({
-        mongoUrl : process.env.MONGODB_URI
+        mongoUrl : process.env.MONGO_URI
     }),
 }));
 
